844930219e8e8ee592a516c1777fbc779208c375
Andreas Triantafyllos <andreas_triantafyllos@hotmail.com>
2021-10-25 19:49:48 +0200

    Fixed median's calculation.

--- src/c-version/util.c
+++ src/c-version/util.c
@@ -14,11 +14,11 @@
 
 
 int comparatorDoubleAsc (const void * a, const void * b) {
-    // if (*(double*)a > *(double*)b)
-    //     return 1;
-    // else if (*(double*)a < *(double*)b)
-    //     return -1;
-    // else
+    if (*(double*)a > *(double*)b)
+        return 1;
+    else if (*(double*)a < *(double*)b)
+        return -1;
+    else
         return 0;
 }
 
